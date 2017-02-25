a4aeb670aa69ba9b1e6de1a5df589c52b7e4c084
Plamen5kov <plamen.krasimirov.petkov@gmail.com>
2017-02-25 22:30:38 +0200

    changed functionality of main class

--- src/main/java/com/kalina/Main.java
+++ src/main/java/com/kalina/Main.java
@@ -1,10 +1,11 @@
 package com.kalina;
 
-/**
- * Created by plamen5kov on 2/25/17.
- */
 public class Main {
     public static void main(String[] args ) {
-        System.out.println("asdasds");
+        printPassedArgs(args);
+    }
+
+    private static void printPassedArgs(String[] args) {
+        System.out.println(args[0]);
     }
 }
