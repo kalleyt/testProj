32bede67ea807fa56e3e8e6daed1a73d58440baa
Plamen5kov <plamen.krasimirov.petkov@gmail.com>
2017-02-25 22:18:13 +0200

    added basic java project

---
+++ src/main/java/com/kalina/Main.java
@@ -0,0 +1,10 @@
+package com.kalina;
+
+/**
+ * Created by plamen5kov on 2/25/17.
+ */
+public class Main {
+    public static void main(String[] args ) {
+        System.out.println("asdasds");
+    }
+}
